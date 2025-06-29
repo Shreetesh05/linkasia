cfeb8eadeb6321867d7d993934b2197defe05ece
shreetesh <shreetesh32@gmail.com>
2025-06-29 16:37:33 +0545

    nfkf

--- src/Nav pages/International.tsx
+++ src/Nav pages/International.tsx
@@ -1,5 +1,5 @@
 // src/components/InternationalToursLight.tsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 const InternationalToursLight = () => {
